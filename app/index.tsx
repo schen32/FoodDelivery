ea538272dbcf0139cb2590f3a0a9db2d909b6f5b
Simon Chen <simonc8192@gmail.com>
2026-01-09 01:49:37 -0800

    - card image and text

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,97 @@
-import { offers } from "@/constants";
-import { FlatList, Pressable, StyleSheet, Text } from "react-native";
+import { images, offers } from "@/constants";
+import { Fragment } from "react";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safe}>
       <FlatList
+        contentContainerStyle={styles.flatListContainer}
         data={offers}
-        renderItem={({ item, index }) => (
-          <Pressable>
-            <Text>{item.title}</Text>
-          </Pressable>
-        )}
+        renderItem={({ item, index }) => {
+          const isEven: boolean = index % 2 === 0;
+
+          return (
+            <Pressable
+              style={[
+                styles.cardPressable,
+                { backgroundColor: item.color },
+                isEven ? styles.cardRowReverse : styles.cardRow,
+              ]}
+            >
+              {({ pressed }) => (
+                <Fragment>
+                  <View style={styles.cardView}>
+                    <Image
+                      style={styles.cardImage}
+                      source={item.image}
+                      resizeMode="contain"
+                    ></Image>
+                  </View>
+                  <View style={styles.cardView}>
+                    <Text style={styles.cardText}>{item.title}</Text>
+                    <Image
+                      style={styles.cardArrow}
+                      source={images.arrowRight}
+                      resizeMode="contain"
+                    ></Image>
+                  </View>
+                </Fragment>
+              )}
+            </Pressable>
+          );
+        }}
       ></FlatList>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 15,
+  },
+  flatListContainer: {
+    gap: 20,
+  },
+  cardPressable: {
+    height: 160,
+    borderRadius: 10,
+    padding: 5,
+  },
+  cardRow: {
+    flexDirection: "row",
+  },
+  cardRowReverse: {
+    flexDirection: "row-reverse",
+  },
+  cardView: {
+    height: "100%",
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cardImage: {
+    height: "100%",
+    width: "100%",
+  },
+  cardText: {
+    color: "white",
+    fontSize: 20,
+    fontFamily: "quicksand-bold",
+    textAlign: "center",
+  },
+  cardArrow: {
+    height: "25%",
+    width: "25%",
+  },
+});
