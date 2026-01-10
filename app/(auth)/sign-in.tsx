465bcafd4936b94cff2e5733bbd363591ff6f786
Simon Chen <simonc8192@gmail.com>
2026-01-09 21:36:26 -0800

    - link to sign up page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,13 @@
 import CustomButton from "@/components/customButton";
 import CustomInput from "@/components/customInput";
+import { colors } from "@/theme/colors";
+import { Link } from "expo-router";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const signIn = () => {
   return (
-    <View style={styles.view}>
+    <View style={styles.inputView}>
       <CustomInput
         placeholder="Enter your email"
         value={""}
@@ -24,6 +26,13 @@ const signIn = () => {
       ></CustomInput>
 
       <CustomButton title="Sign In"></CustomButton>
+
+      <View style={styles.signUpView}>
+        <Text style={styles.signUpText}>Don't have an account?</Text>
+        <Link href="/sign-up" style={styles.signUpLink}>
+          Sign Up
+        </Link>
+      </View>
     </View>
   );
 };
@@ -31,11 +40,29 @@ const signIn = () => {
 export default signIn;
 
 const styles = StyleSheet.create({
-  view: {
+  inputView: {
     gap: 20,
     backgroundColor: "white",
     borderRadius: 20,
     padding: 10,
     marginTop: 20,
+  },
+
+  signUpView: {
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  signUpText: {
+    color: "#6b7280",
+    fontSize: 16,
+    fontFamily: "quicksand-regular",
+  },
+  signUpLink: {
+    color: colors.primary,
+    fontSize: 16,
+    fontFamily: "quicksand-bold",
   },
 });
