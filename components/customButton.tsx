66a42dde2441710c6e62a592b759563808ff5441
Simon Chen <simonc8192@gmail.com>
2026-01-09 21:12:25 -0800

    - custom button

--- components/customButton.tsx
+++ components/customButton.tsx
@@ -1,14 +1,56 @@
+import { colors } from "@/theme/colors";
+import { CustomButtonProps } from "@/type";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-const customButton = () => {
+const customButton = ({
+  onPress,
+  title = "Click Me",
+  style,
+  textStyle,
+  leftIcon,
+  isLoading = false,
+}: CustomButtonProps) => {
   return (
-    <View>
-      <Text>customButton</Text>
-    </View>
+    <TouchableOpacity style={styles.customBtn} onPress={onPress}>
+      {leftIcon}
+
+      <View style={styles.loadingView}>
+        {isLoading ? (
+          <ActivityIndicator size="small" color="white"></ActivityIndicator>
+        ) : (
+          <Text style={styles.loadingText}>{title}</Text>
+        )}
+      </View>
+    </TouchableOpacity>
   );
 };
 
 export default customButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  customBtn: {
+    backgroundColor: colors.primary,
+    borderRadius: 9999, // fully rounded pill
+    padding: 12, // 0.75rem = 12px
+    width: "100%",
+    alignItems: "center", // RN uses alignItems, not justify-content for horizontal centering
+    justifyContent: "center",
+  },
+  loadingView: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loadingText: {
+    color: "white",
+    fontSize: 16,
+    fontFamily: "quicksand-semibold",
+  },
+});
