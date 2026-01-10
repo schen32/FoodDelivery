9d0b698908d748fe82d0a5e8230e28b5e7dc4f29
Simon Chen <simonc8192@gmail.com>
2026-01-10 02:17:32 -0800

    - submit email and password skeleton code

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,17 +1,39 @@
 import CustomButton from "@/components/customButton";
 import CustomInput from "@/components/customInput";
 import { colors } from "@/theme/colors";
-import { Link } from "expo-router";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
 
 const signIn = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const submit = async () => {
+    if (!form.email || !form.password)
+      Alert.alert("Error", "Enter valid email address and password");
+
+    setIsSubmitting(true);
+    try {
+      // call appwrite sign in
+
+      Alert.alert("Success", "User signed in successfully");
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <View style={styles.inputView}>
       <CustomInput
         placeholder="Enter your email"
-        value={""}
-        onChangeText={() => {}}
+        value={form.email}
+        onChangeText={(text) => {
+          setForm((prev) => ({ ...prev, email: text }));
+        }}
         label="Email"
         secureTextEntry={false}
         keyboardType="email-address"
@@ -19,13 +41,19 @@ const signIn = () => {
 
       <CustomInput
         placeholder="Enter your password"
-        value={""}
-        onChangeText={() => {}}
+        value={form.password}
+        onChangeText={(text) => {
+          setForm((prev) => ({ ...prev, password: text }));
+        }}
         label="Password"
         secureTextEntry={true}
       ></CustomInput>
 
-      <CustomButton title="Sign In"></CustomButton>
+      <CustomButton
+        title="Sign In"
+        isLoading={isSubmitting}
+        onPress={submit}
+      ></CustomButton>
 
       <View style={styles.signUpView}>
         <Text style={styles.signUpText}>Don't have an account?</Text>
